bc2449fdf4ba6fdd73a669db08fce84d1be4fedd
HSabbar <36317041+HSabbar@users.noreply.github.com>
2020-02-12 10:47:30 +0100

    Add files via upload

---
+++ pi-face-recognition/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
--- pi-face-recognition/test.py
+++ pi-face-recognition/test.py
@@ -1,1 +1,13 @@
+import numpy as np
+import cv2
 
+# Load an color image in grayscale
+
+
+img = cv2.imread("dataset/hammou/hammou2.png")
+
+# show image
+if img is not None:
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
